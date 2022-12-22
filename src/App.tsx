92c6ef5f5865bd377a82d3833e42739db8171f87
lamba <programmer.lamba@gmail.com>
2022-12-22 06:22:55 +0200

    basic home page completed without respnsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
 import React from 'react';
+import Header from "./components/Header";
 
 function App() {
   return (
-      <>
-      </>
+      <div>
+
+          <img style={{position: "absolute", width: 200, opacity: 0.3, top: 250, right: 350}} src="/svgs/union_27.svg"/>
+
+          <img style={{position: "absolute", width: 500, opacity: 0.1, bottom: 0, left: 0}} src="/svgs/union_35.svg"/>
+
+
+          <Header />
+
+          <div className="home">
+              <div className="text">
+                  <h1>
+                      Get Your<br/>
+                      Software system<br/>
+                      From me.
+                  </h1>
+
+                  <div className="underHeader">
+                      <a className="button" href="resume.pdf" download>
+                          Download CV
+                      </a>
+
+                      <p>
+                          from crm/erp to e-commerce to any custom System <br />
+                          passion and innovation fill my heart.
+                      </p>
+                  </div>
+              </div>
+
+              <div>
+                  <img src="/my-picture.png" />
+              </div>
+          </div>
+
+
+      </div>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import ScrollNavigation from 'react-single-page-navigation';
 
-function Header() {
+function Header(props: any) {
 
 	return (
-		<div>
+		<div className="header">
+			<img src="/logos/my-logo-squared-144.png" alt="logo" />
+
+			<div className="navigation">
+				{/*<ScrollNavigation elements={{ TopOfThePage: {}, BottomOfThePage: {} }}>*/}
+				{/*	{({ refs, activeElement, goTo }) => (*/}
+				{/*		<div>*/}
+				{/*			<div ref={refs.TopOfThePage} onClick={() => goTo('BottomOfThePage')}>*/}
+				{/*				Top Element {activeElement === 'TopOfThePage' && 'is active!'}*/}
+				{/*			</div>*/}
+				{/*			<div ref={refs.BottomOfThePage} onClick={() => goTo('TopOfThePage')}>*/}
+				{/*				Bottom Element {activeElement === 'BottomOfThePage' && 'is active!'}*/}
+				{/*			</div>*/}
+				{/*		</div>*/}
+				{/*	)}*/}
+				{/*</ScrollNavigation>*/}
+			</div>
 
 		</div>
 	);
